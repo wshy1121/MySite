7dcdd4e9c6e14c44ffa42bb6aa58765f4276139f
huang_yuan <wshy1121@qq.com>
2015-05-29 22:25:45 +0800

    初始版本提交

---
+++ 1/calc/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+ 
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a)+int(b)
+    return HttpResponse(str(c))
+
+def add2(request,a,b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+
+    
---
+++ 1/mysite/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+ 
+from django.contrib import admin
+admin.autodiscover()
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'learn.views.handleRequest', name='home'),
+	url(r'^add/$', 'calc.views.add', name='add'),
+	url(r'^add/(\d+)/(\d+)/$', 'calc.views.add2', name='add2'),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
